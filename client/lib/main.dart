6587819d5b6722fd4fe544aa212f863af5bd32e5
Banula Perera <93034784+banulaperera@users.noreply.github.com>
2024-02-06 00:08:39 +0530

    initial commit on splash screen

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:client/constats.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,34 +7,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Intellectia',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        brightness: Brightness.light,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBackgroundColor,
         useMaterial3: true,
       ),
-<<<<<<< Updated upstream
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-=======
       home: const SplashScreen(),
     );
   }
@@ -59,10 +42,39 @@ class SplashScreen extends StatelessWidget {
               ),
             ),
           ),
-          const Expanded(child: Column())
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Intellectia\n",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontFamily: 'Poppins',
+                              letterSpacing: 5,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        TextSpan(
+                          text: "'Where education made effortless and fun'",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 13,
+                              fontFamily: 'Poppins',
+                              letterSpacing: 4,
+                              fontStyle: FontStyle.italic),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
->>>>>>> Stashed changes
     );
   }
 }
