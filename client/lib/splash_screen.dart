1f7a8d62af7ab6109e6d24a3d2331bb58bb919be
Banula Perera <93034784+banulaperera@users.noreply.github.com>
2024-02-06 15:05:50 +0530

    adding splash screen package

---
+++ client/lib/splash_screen.dart
@@ -0,0 +1,91 @@
+import 'package:client/constats.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        "assets/undraw_taking_notes_re_bnaf (1) 1.png"),
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Intellectia\n",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 28,
+                            fontFamily: 'Poppins',
+                            letterSpacing: 5,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        WidgetSpan(child: SizedBox(height: 50)),
+                        TextSpan(
+                          text: "'Where education made effortless and fun'",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 13,
+                            fontFamily: 'Poppins',
+                            letterSpacing: 4,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  FittedBox(
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 70),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 90),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: kPrimaryColor,
+                      ),
+                      child: const Row(
+                        children: [
+                          Text(
+                            "Get Started",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18,
+                              letterSpacing: 3,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
