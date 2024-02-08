d652694ad4218c7a41a98ba8186eac59f05555a4
Banula Perera <93034784+banulaperera@users.noreply.github.com>
2024-02-08 22:52:14 +0530

    final commit of splash screen

--- client/lib/signup_page.dart
+++ client/lib/signup_page.dart
@@ -28,22 +28,22 @@ class SignupPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const SizedBox(height: 25),
-                  _buildTextField(
+                  textField(
                     hintText: 'Email',
                     keyboardType: TextInputType.emailAddress,
                   ),
                   const SizedBox(height: 25),
-                  _buildTextField(
+                  textField(
                     hintText: 'Password',
                     keyboardType: TextInputType.visiblePassword,
                   ),
                   const SizedBox(height: 25),
-                  _buildTextField(
+                  textField(
                     hintText: 'Confirm Password',
                     keyboardType: TextInputType.visiblePassword,
                   ),
                   const SizedBox(height: 25),
-                  _buildButton(
+                  button(
                     label: 'Sign Up',
                     onPressed: () {
                       Navigator.push(
@@ -58,71 +58,6 @@ class SignupPage extends StatelessWidget {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTextField(
-      {required String hintText, required TextInputType keyboardType}) {
-    return TextField(
-      style: const TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.bold,
-      ),
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 16,
-          letterSpacing: 2,
-        ),
-        filled: true,
-        fillColor: Colors.white,
-        border: OutlineInputBorder(
-          borderSide:
-              const BorderSide(color: Colors.black, style: BorderStyle.none),
-          borderRadius: BorderRadius.circular(60),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: kPrimaryColor, width: 2.5),
-          borderRadius: BorderRadius.circular(60),
-        ),
-        contentPadding:
-            const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
-      ),
-      keyboardType: keyboardType,
-    );
-  }
-
-  Widget _buildButton(
-      {required String label, required VoidCallback onPressed}) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(60),
-          color: kPrimaryColor,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: const Offset(0, 4),
-            ),
-          ],
-        ),
-        alignment: Alignment.center,
-        child: Text(
-          label,
-          style: const TextStyle(
-            color: Colors.white,
-            fontFamily: 'Poppins',
-            fontWeight: FontWeight.w700,
-            fontSize: 18,
-            letterSpacing: 3,
-          ),
         ),
       ),
     );
