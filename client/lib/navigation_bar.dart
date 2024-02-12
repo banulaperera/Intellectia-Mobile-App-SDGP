37e422e6f0e2e38023462b58268a0ce2c5eb93ab
Hiru-chand <85777025+Hiru-chand@users.noreply.github.com>
2024-02-12 17:07:53 +0530

    updating bootstrap icons instead of iconsax for navigation bar

--- client/lib/navigation_bar.dart
+++ client/lib/navigation_bar.dart
@@ -1,14 +1,10 @@
+import 'package:bootstrap_icons/bootstrap_icons.dart';
 import 'package:client/constants.dart';
-import 'package:client/signup_page.dart';
-import 'package:client/splash_screen.dart';
+import 'package:client/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
-
-import 'login_page.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({super.key});
-
   @override
   State<BottomNavigation> createState() => _BottomNavigation();
 }
@@ -16,50 +12,57 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigation extends State<BottomNavigation> {
   int _currentNavigationIndex = 0;
 
-  //Testing data
+//Testing data
+
   final pages = [
-    const LoginPage(),
-    const SignupPage(),
-    const SplashScreen(),
-    const Text('data')
+    const HomePage(),
+
+    const Text('data'),
+
+    const Text('data'),
+
+// const NotificationScreen(),
+
+    const Text('data'),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[_currentNavigationIndex],
+        body: pages[_currentNavigationIndex],
         bottomNavigationBar: BottomNavigationBar(
-      currentIndex: _currentNavigationIndex,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.white70,
-      selectedFontSize: 13,
-      selectedIconTheme: const IconThemeData(size: 30, shadows: <Shadow>[Shadow(color: Colors.white, blurRadius: 25.0)],),
-      unselectedIconTheme: const IconThemeData(size: 23),
-      backgroundColor: kPrimaryColor,
-      elevation: 10,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Iconsax.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Iconsax.cup),
-          label: 'Goal',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Iconsax.notification),
-          label: 'Notification',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Iconsax.user),
-          label: 'Profile',
-        ),
-      ],
-      onTap: (index) {
-        setState(() {
-          _currentNavigationIndex = index;
-        });
-      },
-    ));
+          currentIndex: _currentNavigationIndex,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.black54,
+          selectedFontSize: 13,
+          selectedIconTheme: const IconThemeData(size: 30, color: Colors.white),
+          unselectedIconTheme:
+              const IconThemeData(size: 23, color: Colors.black45),
+          backgroundColor: kPrimaryColor,
+          elevation: 10,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(BootstrapIcons.house_fill),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(BootstrapIcons.bullseye),
+              label: 'Goal',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(BootstrapIcons.bell_fill),
+              label: 'Notification',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(BootstrapIcons.person_fill),
+              label: 'Profile',
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentNavigationIndex = index;
+            });
+          },
+        ));
   }
 }
