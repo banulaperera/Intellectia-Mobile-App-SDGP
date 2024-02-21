5a70b161dd3ae1d7dceab5d7af32c7f1b7bfc30b
nethunilfernando <lakshikanethunil@gmail.com>
2024-02-21 22:34:31 +0530

    quiz page commit

---
+++ client/lib/screens/quiz/quiz_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/Material.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:client/controllers/question_controller.dart';
+ 
+import 'components/body.dart';
+ 
+class QuizScreen extends StatelessWidget {
+  const QuizScreen({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    QuestionController controller = Get.put(QuestionController());
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        // Fluttter show the back button automatically
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          FloatingActionButton(
+            backgroundColor: Colors.blue,
+            onPressed: controller.nextQuestion,
+            child: const Text("Skip"),
+          ),
+        ],
+      ),
+      body: const Body(<dynamic>{}),
+    );
+  }
+}
---
+++ client/lib/screens/quiz/welcome/welcome_screen.dart
@@ -0,0 +1,84 @@
+import 'package:client/screens/quiz/quiz_screen.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
+// import 'package:get/get.dart';
+import 'package:client/constants.dart';
+ 
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(flex: 2), //2/6
+                  Text(
+                    "Are you ready for the quiz?",
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: const Color.fromARGB(255, 6, 6, 6),
+                        fontWeight: FontWeight.bold),
+                  ),
+                  // const Text("Enter your information below"),
+                  // const Spacer(), // 1/6
+                  // const TextField(
+                  //   decoration: InputDecoration(
+                  //     filled: true,
+                  //     fillColor: Color.fromARGB(255, 249, 249, 249),
+                  //     hintText: "Full Name",
+                  //     border: OutlineInputBorder(
+                  //       borderRadius: BorderRadius.all(Radius.circular(12)),
+                  //     ),
+                  //   ),
+                  // ),
+                  const Spacer(), // 1/6
+                  // InkWell(
+                  //   onTap: () => Get.to(const QuizScreen()),
+                  //   child: Container(
+                  //     width: double.infinity,
+                  //     alignment: Alignment.center,
+                  //     padding:
+                  //         const EdgeInsets.all(kDefaultPadding * 0.75), // 15
+                  //     decoration: const BoxDecoration(
+                  //       gradient: kPrimaryGradient,
+                  //       borderRadius: BorderRadius.all(Radius.circular(12)),
+                  //     ),
+                  //     child: Text(
+                  //       "Let's Start Quiz",
+                  //       style: Theme.of(context)
+                  //           .textTheme
+                  //           .labelLarge
+                  //           ?.copyWith(color: Colors.black),
+                  //     ),
+                  //   ),
+                  // ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const QuizScreen()),
+                      );
+                    },
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.black,
+                    hoverColor: Colors.blueGrey,
+                    child: const Text('Start'),
+                  ),
+                  const Spacer(flex: 2), // it will take 2/6 spaces
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
