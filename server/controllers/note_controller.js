149587f45b1f75e1035651410b16aac6ea170f1c
Avishka Pramuditha <avishkapramuditha28@gmail.com>
2024-03-01 12:58:03 +0530

    NOTE API STATUS CODE FIXED

--- server/controllers/note_controller.js
+++ server/controllers/note_controller.js
@@ -1,12 +1,12 @@
 const NoteService=require('../services/note_service');
-const e = require("express");
+
 
 exports.addNote=async (req,res)=>{
     try {
         const {userID}=req.user
         const note=req.body
         await NoteService.addNote(userID,note);
-        res.json({status: 200, Message: "The Note Successfully Added"})
+        res.status(200).json({ Message: "The Note Successfully Added"})
     }catch (error){
         res.status(400).json({ Message: "The request is not completed "})
     }
@@ -16,7 +16,7 @@ exports.getAllNote=async (req,res)=>{
     try {
         const {userID}=req.user
         const notes=await NoteService.getAllNotes(userID);
-        res.json({status: 200,notes});
+        res.status(200).json({notes});
     }catch (error){
         res.status(400).json({ Message: "The request is not completed "})
     }
@@ -27,7 +27,7 @@ exports.deleteNote=async (req,res)=>{
          const {noteID}=req.params;
          const {userID}=req.user;
          await NoteService.deleteNote(userID,noteID)
-         res.json({status: 200, Message: "The Note Successfully Deleted"})
+        res.status(200).json({Message: "The Note Successfully Deleted"})
     }catch (error){
         res.status(400).json({ Message: "The request is not completed "})
     }
@@ -38,7 +38,7 @@ exports.updateNote=async (req,res)=>{
         const note=req.body;
         const {userID}=req.user;
         await NoteService.updateNote(userID,note);
-        res.json({status: 200, Message: "The Note Successfully updated"})
+        res.status(200).json({Message: "The Note Successfully updated"})
     }catch (error){
         console.log(error)
         res.status(400).json({ Message: "The request is not completed "})
