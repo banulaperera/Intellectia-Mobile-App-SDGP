f940654e1cc7f83cdb194daa64511b14bb97fd39
Avishka Pramuditha <avishkapramuditha28@gmail.com>
2024-03-06 15:57:28 +0530

    QUIZ API METHOD CHANGED GET TO POST

--- server/routes/quiz_routes.js
+++ server/routes/quiz_routes.js
@@ -3,6 +3,6 @@ const router=express.Router();
 const auth=require('../middleware/auth');
 const noteController=require('../controllers/quiz_controller');
 
-router.get('/generate',auth,noteController.generateQuiz);
+router.post('/generate',auth,noteController.generateQuiz);
 module.exports=router;
 
