1104b22832ea912f248a1cd1eb6b9821b9901132
Avishka Pramuditha <avishkapramuditha28@gmail.com>
2024-03-18 23:28:40 +0530

    NOTIFICATION SERVICE ADDED

---
+++ client/lib/util/notification_service.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+
+class NotificationService {
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin=FlutterLocalNotificationsPlugin();
+    
+    Future<void> initializeNotification() async{
+      const AndroidInitializationSettings androidInitializationSettings=AndroidInitializationSettings('@mipmap/ic_launcher');
+      const InitializationSettings initializationSettings=InitializationSettings(android: androidInitializationSettings);
+      await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    }
+
+    Future<void> showNotification() async{
+      const AndroidNotificationDetails androidNotificationDetails=AndroidNotificationDetails("channel1","notification channel",importance: Importance.max,priority: Priority.max);
+      const NotificationDetails notificationDetails=NotificationDetails(android: androidNotificationDetails);
+
+      await flutterLocalNotificationsPlugin.show(0,"first", "hi how are you",notificationDetails);
+    }
+
+
+
+}
+
+
