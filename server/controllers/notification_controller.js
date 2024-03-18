5d8a2d168f4630064395e191c654c41209bf39a3
Avishka Pramuditha <avishkapramuditha28@gmail.com>
2024-03-18 16:17:32 +0530

    YOUTUBE DATA API DONE

--- server/controllers/notification_controller.js
+++ server/controllers/notification_controller.js
@@ -1,4 +1,9 @@
 const NotificationService=require('../services/notification_service');
+const NoteService=require('../services/note_service');
+const {google} =require('googleapis');
+const {content} = require("googleapis/build/src/apis/content");
+
+
 
 exports.getAllNotification=async (req,res)=>{
      try {
@@ -32,4 +37,57 @@ exports.deleteNotification=async (req,res)=>{
     }catch (error){
         res.status(400).json({ Message: "The request is not completed "});
     }
+}
+
+exports.getYoutubeNotification=async (req,res)=>{
+    try {
+        const {userID}=req.user;
+        const moduleNames= await getUserNoteModuleNames(userID);
+
+        if (moduleNames.length!==0){
+            let index=Math.floor((Math.random()*(moduleNames.length)));
+            const content=await getRandomNoteContent(userID,moduleNames[index]);
+            const link= await getYoutubeVideoLink(moduleNames[index]+" "+content);
+            res.status(200).json({"link":link,"module":moduleNames[index]});
+
+        }
+    }catch (e) {
+        console.log(e)
+        res.status(400).json({ Message: "The request is not completed "});
+    }
+}
+
+async  function getUserNoteModuleNames(userID){
+ let notes=await NoteService.getAllNotes(userID);
+ let moduleNames=notes.map(note=>note.moduleName);
+ return Array.from(new Set(moduleNames));
+}
+
+
+async function getRandomNoteContent(userID,moduleName){
+    let notes=await NoteService.getAllNotes(userID);
+    const noteArray=notes.filter(note=>note.moduleName===moduleName);
+    let index=Math.floor((Math.random()*(noteArray.length)));
+    return noteArray[index].title+" "+noteArray[index].content;
+}
+
+  async function getYoutubeVideoLink(query){
+
+    const youtube= google.youtube({version:'v3',auth:process.env.YOUTUBE_API_KEY});
+    const params={part:'snippet',type:'video',maxResults:10,q:`Educational video for ${query} Module`};
+
+    try {
+        const res=await youtube.search.list(params);
+        const index = Math.floor((Math.random() * 5));
+        const videoID=res.data.items[index]?.id.videoId;
+        if (videoID){
+            return `https://www.youtube.com/watch?v=${videoID}`;
+        }else {
+            return  null;
+        }
+
+    }catch (error){
+        throw error;
+    }
+
 }
--- server/routes/notification_routes.js
+++ server/routes/notification_routes.js
@@ -4,6 +4,7 @@ const notificationController=require('../controllers/notification_controller');
 const auth=require('../middleware/auth');
 
 router.get('/all',auth,notificationController.getAllNotification);
+router.get('/youtube-link',auth,notificationController.getYoutubeNotification);
 router.post('/add',auth,notificationController.addNotification);
 router.delete('/delete/:notificationID',auth,notificationController.deleteNotification);
 
