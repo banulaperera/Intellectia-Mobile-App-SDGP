04b38d037ba44141e78211582f60bb011283885f
Avishka Pramuditha <avishkapramuditha28@gmail.com>
2024-03-22 16:50:54 +0530

    QUIZ SCHEDULE DETAILS API DONE

--- server/controllers/quiz_controller.js
+++ server/controllers/quiz_controller.js
@@ -1,65 +1,55 @@
 const quizService=require('../services/quiz_service');
+const http = require("http");
 
 exports.generateQuiz=async (req,res)=>{
     try {
-        // const {content}=req.body;
-        // const quizString=await quizService.generateQuiz(content);
-        // const quiz=JSON.parse(quizString);
-        const temp = {
-            "questions": [
-                {
-                    "question": "What is encapsulation in object-oriented programming?",
-                    "options": [
-                        "A way to restrict access to all variables within an object",
-                        "A way to allow direct access to all variables within an object",
-                        "A way to hide data members and data functions of an object",
-                        "A way to expose all data members and data functions of an object"
-                    ],
-                    "answer": "A way to hide data members and data functions of an object"
-                },
-                {
-                    "question": "What does encapsulation help achieve in software development?",
-                    "options": [
-                        "Improved security",
-                        "Increased complexity",
-                        "Easier debugging",
-                        "Faster execution"
-                    ],
-                    "answer": "Improved security"
-                },
-                {
-                    "question": "Which of the following is a benefit of encapsulation?",
-                    "options": [
-                        "Increased coupling between classes",
-                        "Decreased reusability of code",
-                        "Enhanced code organization",
-                        "Limited data hiding"
-                    ],
-                    "answer": "Enhanced code organization"
-                },
-                {
-                    "question": "In encapsulation, what is used to restrict access to certain components of an object?",
-                    "options": [
-                        "Inheritance",
-                        "Polymorphism",
-                        "Abstraction",
-                        "Access modifiers"
-                    ],
-                    "answer": "Access modifiers"
-                },
-                {
-                    "question": "What is encapsulation commonly associated with in object-oriented programming?",
-                    "options": [
-                        "Data hiding",
-                        "Data duplication",
-                        "Data exposure",
-                        "Data destruction"
-                    ],
-                    "answer": "Data hiding"
-                }
-            ]
-        }
-        res.status(200).json(temp);
+        const {content}=req.body;
+        const quizString=await quizService.generateQuiz(content);
+        const quiz=JSON.parse(quizString);
+        res.status(200).json(quiz);
+    }catch (error){
+        res.status(400).json({ Message: "The request is not completed "})
+    }
+}
+
+exports.addScheduledQuizDetails=async (req,res)=>{
+    try {
+        const {userID}=req.user;
+        const scheduledQuizDetails=req.body;
+        await quizService.addScheduleQuizDetails(userID,scheduledQuizDetails);
+        res.status(200).json({ Message: "The Quiz Successfully Scheduled"})
+    }catch (error){
+        res.status(400).json({ Message: "The request is not completed "})
+    }
+}
+
+exports.getAllScheduledQuizDetails=async (req,res)=>{
+    try {
+        const {userID}=req.user;
+        const  scheduleDetails = await quizService.getAllScheduledQuizDetails(userID);
+        res.status(200).json({scheduleDetails});
+    }catch (error){
+        res.status(400).json({ Message: "The request is not completed "})
+    }
+}
+
+exports.deleteScheduledQuizDetails=async (req,res)=>{
+    try {
+        const {userID}=req.user;
+        const {scheduledQuizDetailsID}=req.params;
+        await quizService.deleteScheduledQuizDetails(userID,scheduledQuizDetailsID);
+        res.status(200).json({Message: "The Quiz Preference Deleted"})
+    }catch (error){
+        res.status(400).json({ Message: "The request is not completed "})
+    }
+}
+
+exports.updateScheduledQuizDetails=async (req,res)=>{
+    try {
+        const {userID}=req.user;
+        const scheduledQuizDetails=req.body;
+        await quizService.updateScheduledQuizDetails(userID,scheduledQuizDetails);
+        res.status(200).json({Message: "The Quiz Preference Changed"})
     }catch (error){
         res.status(400).json({ Message: "The request is not completed "})
     }
--- server/services/quiz_service.js
+++ server/services/quiz_service.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const {OpenAI} = require('openai')
+const User = require("../model/User");
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 exports.generateQuiz = async function (content) {
@@ -21,4 +22,42 @@ exports.generateQuiz = async function (content) {
 
 }
 
+exports.addScheduleQuizDetails = async function (userID,scheduledQuizDetails){
+    try{
+        const user=await User.findOne({_id:userID});
+        user.scheduledQuiz.push(scheduledQuizDetails);
+        await user.save();
+    }catch (error){
+        throw error;
+    }
+}
 
+exports.getAllScheduledQuizDetails=async function (userID){
+    try {
+        const user=await User.findOne({_id:userID});
+        return user.scheduledQuiz;
+    }catch (error){
+        throw Error
+    }
+}
+
+exports.deleteScheduledQuizDetails= async function (userID,scheduledQuizDetailsID){
+    try{
+        const user=await User.findOne({_id:userID});
+        user.scheduledQuiz.pull({_id:scheduledQuizDetailsID})
+        await user.save();
+    }catch (error){
+        throw error
+    }
+}
+
+exports.updateScheduledQuizDetails= async function (userID,scheduledQuizDetails){
+    try{
+        await User.findOneAndUpdate(
+            {'scheduledQuiz._id':scheduledQuizDetails._id,_id:userID},
+            {$set:{'scheduledQuiz.$':scheduledQuizDetails}}
+        )
+    }catch (error) {
+        throw error;
+    }
+}
