e0b05fdeb0a924e08488d5b33154c288a0c44744
Avishka Pramuditha <avishkapramuditha28@gmail.com>
2024-03-23 11:09:24 +0530

    QUIZ API CHANGED

--- server/routes/quiz_routes.js
+++ server/routes/quiz_routes.js
@@ -5,9 +5,7 @@ const quizController=require('../controllers/quiz_controller');
 
 router.post('/generate',auth,quizController.generateQuiz);
 router.post('/schedule/add-details',auth,quizController.addScheduledQuizDetails);
-router.get('/schedule/getAll-details',auth,quizController.getAllScheduledQuizDetails);
-router.delete('/schedule/delete-details/:scheduledQuizDetailsID',auth,quizController.deleteScheduledQuizDetails);
-router.put('/schedule/update-details',auth,quizController.updateScheduledQuizDetails);
+router.get('/schedule/get-details',auth,quizController.getScheduledQuizDetails);
 
 module.exports=router;
 
